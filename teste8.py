4254a84a5d41820ff976cc0c914bfa73c7f217a6
João Lucas <86527954+Lucassz18@users.noreply.github.com>
2022-05-31 07:42:20 -0300

    Update teste8.py

--- teste8.py
+++ teste8.py
@@ -12,7 +12,7 @@ b = str(input("Digite o auto: "));
 c = str(input("Digite o CEP: "));
 d = str(input("Digite o endereço: "));
 e = str(input("Digite o número da casa: "));
-f = str(input("Possui complemento: [S/N]\n").lower());
+f = str(input("Possui complemento: [S/N]\").lower());
 if f == "s":
     g = str(input("Digite o complemento: "))
     print(f'autuado: {a},auto: {b}, CEP: {c}, endereço: {d}, número da casa: {e}, complemento: {g}\n\n')
